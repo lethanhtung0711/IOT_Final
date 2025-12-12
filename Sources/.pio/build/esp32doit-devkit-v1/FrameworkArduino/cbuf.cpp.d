e83630e805b33539c69448033a06c6dd331c8a7b
lethanhtung0711 <lethanhtung_t67@hus.edu.vn>
2025-12-12 10:42:54 +0700

    upload README

---
+++ Sources/.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/tungt/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/tungt/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ Sources/.pio/build/esp32doit-devkit-v1/lib34c/Ticker/Ticker.cpp.d
@@ -0,0 +1,6 @@
+.pio/build/esp32doit-devkit-v1/lib34c/Ticker/Ticker.cpp.o: \
+ C:/Users/tungt/.platformio/packages/framework-arduinoespressif32/libraries/Ticker/src/Ticker.cpp \
+ C:/Users/tungt/.platformio/packages/framework-arduinoespressif32/libraries/Ticker/src/Ticker.h \
+ C:/Users/tungt/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_timer/include/esp_timer.h \
+ C:/Users/tungt/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ C:/Users/tungt/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h
